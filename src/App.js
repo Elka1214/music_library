ec8f69219a9d35809b75424922bce770661eea72
Elka1214 <116694312+Elka1214@users.noreply.github.com>
2023-04-22 10:38:48 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Gallery from "./Components/Gallery";
+import Searchbar from "./Components/Searchbar";
+import { DataContext } from "./Contexts/DataContext";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [message, setMessage] = useState("Search for music!");
+  const [data, setData] = useState([]);
+
+  const API_URL = "https://itunes.apple.com/search?term=";
+
+  useEffect(() => {
+    if (search) {
+      const fetchData = async () => {
+        const URL = encodeURI(API_URL + search);
+        const response = await fetch(URL);
+        const data = await response.json();
+        if (data.results.length > 0) {
+          setData(data.results);
+        } else {
+          setMessage("Not found");
+          setData([]);
+        }
+      };
+      fetchData();
+    }
+  }, [search]);
+
+  const handleSearch = (e, term) => {
+    e.preventDefault();
+    setSearch(term);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Searchbar handleSearch={handleSearch} />
+      {message}
+      <DataContext.Provider value={data}>
+        <Gallery />
+      </DataContext.Provider>
     </div>
   );
 }
---
+++ src/Components/Searchbar.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Searchbar(props) {
+  const [search, setSearch] = useState("");
+  return (
+    <form onSubmit={(e) => props.handleSearch(e, search)}>
+      <input
+        onChange={(e) => setSearch(e.target.value)}
+        value={search}
+        type="text"
+        placeholder="Enter a search term here"
+      />
+      <input type="submit" />
+    </form>
+  );
+}
+
+export default Searchbar;
